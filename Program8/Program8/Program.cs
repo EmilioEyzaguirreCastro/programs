086bb2100f0342da8993365d2bd7052eff58fa0c
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:52:02 -0500

    Add files via upload

---
+++ Program8/Program8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número para hallarle la raíz cuadrada:");
+            double sqr = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(sqr));
+        }
+    }
+}

5fad314e6c78a8304f6dce49e2747a3485dc9028
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:56:08 -0500

    Add files via upload

---
+++ Program11/Program11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 2 números para hallar el mayor de los dos:");
+            double mx = Convert.ToDouble(Console.ReadLine());
+            double mx1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Max(mx, mx1));
+        }
+    }
+}

b5ef2c35772b888621cad4a8987c348fb3805863
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:36:01 -0500

    Add files via upload

---
+++ Program6/Program6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar 2 numeros a multiplicar:");
+            double sum1 = Convert.ToDouble(Console.ReadLine());
+            double sum2 = Convert.ToDouble(Console.ReadLine());
+            double answer = sum1 * sum2;
+            Console.WriteLine("El resultado es: " + answer);
+        }
+    }
+}

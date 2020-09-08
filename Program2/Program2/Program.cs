92053dfd3b1d022d6eeeb9b7c799041dd5710bbc
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:29:57 -0500

    Add files via upload

---
+++ Program2/Program2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Welcome " + username);
+        }
+    }
+}

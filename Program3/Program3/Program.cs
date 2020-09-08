2b539f8eaccecffcdbf6198477d1d9b9df311043
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:31:26 -0500

    Add files via upload

---
+++ Program3/Program3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Ask the age
+            Console.WriteLine("Enter your age:");
+
+            //Store the answer on the variable "age"(first convert it)
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //Say your age
+            Console.WriteLine("Your age is " + age);
+        }
+    }
+}

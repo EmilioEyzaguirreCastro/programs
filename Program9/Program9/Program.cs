ca01d84590d89224b81481b63885911ce6f79703
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:52:51 -0500

    Add files via upload

---
+++ Program9/Program9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número para redondearlo al entero mas cercano:");
+            double rnd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Round(rnd));
+        }
+    }
+}

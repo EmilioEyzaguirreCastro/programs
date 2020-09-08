69d2a14d9d62b530402f1ca7ff69a3c9633637e0
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-08 11:54:07 -0500

    Add files via upload

---
+++ Program10/Program10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 2 números para hallar el menor de los dos:");
+            double mn = Convert.ToDouble(Console.ReadLine());
+            double mn1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Min(mn, mn1));
+        }
+    }
+}

0cecf071a2e24ce973e46c47c2a80d5185abffc6
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-09 15:31:37 -0500

    Add files via upload

---
+++ Program12/Program12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la contraseña correcta(buscala en about.docx):");
+            Console.Write("Contraseña: ");string pwd = Console.ReadLine(); string answer = "thepassword";
+            if (pwd == answer) { Console.WriteLine("Bienvenido");}
+            else{ Console.WriteLine("Contraseña incorrecta. Inténtelo de nuevo");}
+        }
+    }
+}

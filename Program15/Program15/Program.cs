15beebaa231f6b8715b36b637626aba4dda5a4d4
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2020-09-09 15:35:35 -0500

    Add files via upload

---
+++ Program15/Program15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba algo: un número o un texto. Recomendamos probar ambos");
+            try
+            {
+                int err = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escribiste " + err);
+            }
+            catch (Exception e) { Console.WriteLine("Oh no. Something went wrong. Try to write a not too long number"); }
+        }
+    }
+}
